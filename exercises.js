9e6981a12c34441fd567ba3524bec993d8d531f7
zakdrummond <zakdrummond@gmail.com>
2018-01-17 08:52:38 -0800

    first exercise completed

---
+++ exercises.js
@@ -0,0 +1,20 @@
+// In this drill, you need to write code for the createMyObject function so that it returns an object with the following key value pairs:
+
+// foo => 'bar'
+// answerToUniverse => 42
+// olly olly => 'oxen free'
+// sayHello => a function that returns the string 'hello'
+'use strict';
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    }  
+  };
+}
+
+console.log(createMyObject());
