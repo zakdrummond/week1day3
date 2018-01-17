95c2e8da527619faa30d656d2a240588f09ccff2
Chris Yang <cyang287@yahoo.com>
2018-01-17 14:42:59 -0500

    add validateKeys

--- exercises.js
+++ exercises.js
@@ -194,3 +194,46 @@ function testIt() {
 }
 
 testIt();
+
+
+
+// Spend up to 20 minutes writing a function called validateKeys. This function takes 2 arguments, object and expectedKeys. 
+// object is (you guessed it!) an object that we want to validate keys for. 
+// expectedKeys is an array of keys that we expect to find on the object.
+
+// validateKeys should return true if object has all of the keys from expectedKeys, and no additional keys.
+// It should return false if one or more of the expectedKeys is missing from the object, or if the object contains extra keys not in expectedKeys.
+
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+// running the function with `objectB` and `expectedKeys`
+// should return `false`
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  let compare = Object.keys(object);
+  if (compare.length !== expectedKeys.length) {
+    return false;
+  }
+  for (let i = 0; i < expectedKeys.length; i++) {
+    if (expectedKeys[i] !== compare[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(validateKeys(objectA, expectedKeys));
