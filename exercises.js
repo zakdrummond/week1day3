f4cab3d833d101e1499d3ef153b89988d5c497c6
Chris Yang <cyang287@yahoo.com>
2018-01-17 12:12:14 -0500

    add keyDeleter function

--- exercises.js
+++ exercises.js
@@ -56,3 +56,20 @@ function personMaker() {
 }
 
 console.log(personMaker());
+
+// Modify the keyDeleter function so that it deletes keys for foo and bar for any object passed in and then returns the modified object.
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+console.log(keyDeleter(sampleObj));
